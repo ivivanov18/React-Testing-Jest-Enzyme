1630c626865343cfc1c020a1c2eef8b16f605aef
ivivanov18 <ivanov.ivan.v@gmail.com>
2019-03-31 16:00:35 +0300

    add and init redux

---
+++ src/reducers/successReducer.js
@@ -0,0 +1,9 @@
+/**
+ * Function successReducer
+ * @param {array} state - array of guessed Words
+ * @param {object} action - action to be reduced
+ * @returns {boolean} - new success state
+ */
+export default (state, action) => {
+  return null;
+};
