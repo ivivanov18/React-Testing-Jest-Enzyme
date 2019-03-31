b96b2c8728c30b8fc16e3fc42753f96d565ec7eb
ivivanov18 <ivanov.ivan.v@gmail.com>
2019-03-31 15:37:34 +0300

    tests and code for GuessWords component for case when words guessed are available

--- src/GuessWords.js
+++ src/GuessWords.js
@@ -7,6 +7,29 @@ const GuessWords = props => {
     contents = (
       <span data-test="guess-instructions">Try to guess the secret word!</span>
     );
+  } else {
+    const guessedWordRows = props.guessedWords.map((word, index) => {
+      return (
+        <tr data-test="guessed-word" key={index}>
+          <td>{word.guessedWord}</td>
+          <td>{word.letterMatchCount}</td>
+        </tr>
+      );
+    });
+    contents = (
+      <div data-test="guessed-words">
+        <h3>Guessed Words</h3>
+        <table>
+          <thead>
+            <tr>
+              <th>Guess</th>
+              <th>Matching Letters</th>
+            </tr>
+          </thead>
+          <tbody>{guessedWordRows}</tbody>
+        </table>
+      </div>
+    );
   }
   return <div data-test="component-guessed-words"> {contents}</div>;
 };
